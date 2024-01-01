9b787129e3441922ab6c454268f613ff85af9c9a
Emre Can Şimşek <emrecan.midyat@gmail.com>
2024-01-02 00:49:12 +0300

    add favoite hotels

--- app/favorite-hotels/page.tsx
+++ app/favorite-hotels/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client"
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import HotelsReducer from "../redux/reducers/HotelsReducer";
+import { HotelModel } from "../models/HotelModel";
+import HotelCard from "../components/HotelCard";
 
 const FavoriteHotels = () => {
+
+    const dispatch = useDispatch<any>()
+    const { hotelsResponse } = useSelector((state: RootState) => state.hotels)
+
+    const onPressFavorite = (id: number) => {
+        dispatch(HotelsReducer.changeFavorite(id))
+    }
+
     return (
-        <div>Favorite Hotels</div>
+        <div>
+            {
+                hotelsResponse &&
+                <div className="grid 2xl:grid-cols-6 lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 min-h-[632px]">
+                    {
+                        hotelsResponse.hotels.map((hotel: HotelModel) => {
+                            if (hotel.isFavorite === true) {
+                                return <HotelCard key={hotel.id} id={hotel.id} image={hotel.photo} name={hotel.name} location={hotel.location} price={hotel.price} isFavorite={hotel.isFavorite} onPressFavorite={onPressFavorite} />
+                            }else {
+                                return null
+                            }
+                        }
+                        )
+                    }
+                </div>
+            }
+        </div>
     )
 }
 
